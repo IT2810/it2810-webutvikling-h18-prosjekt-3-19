f8c25351e084f9cf2fe3a6c016d20c41eb4ad414
Ahsan Azim <ahsan.azim@hotmail.com>
2018-10-16 15:20:28 +0200

    #5 & #6  Created the maps page and component
    Issue #6  and #5
    Need to make tweaks to the map

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import FetchLocation from "../components/FetchLocation";
+import UsersMap from "../components/UsersMap";
 
 export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
+  state = {
+    userLocation: null,
+    usersPlaces: []
   };
 
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          userLocation: {
+            latitude: 63.416,
+            longitude: 10.405,
+            latitudeDelta: 0.0622,
+            longitudeDelta: 0.0421
+          }
+        });
+        fetch("me", {
+          method: "POST",
+          body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        })
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+      },
+      err => console.log(err)
+    );
+  };
+
+  
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation={this.getUserLocationHandler} />
+        <UsersMap
+          userLocation={this.state.userLocation}
+          usersPlaces={this.state.usersPlaces}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
