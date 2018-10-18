66490862c45027b20a3fe47ca1fb5b7848af7021
Unknown <paul.fossnes@gmail.com>
2018-10-18 11:57:42 +0200

    #3 Todo unit-test and Main unit-test expanded

--- components/__tests__/Main-test.js
+++ components/__tests__/Main-test.js
@@ -1,39 +1,37 @@
 import React from "react";
 import Main from "../Main";
 import renderer from "react-test-renderer";
-/*
+
 describe("Testing Main", () => {
-  const tree = renderer.create(<Main />).getInstance();
-
-  const initialState = {
-    noteArray: [],
-    noteText: "initialText"
-  };
-*/
-test("renders correctly", () => {
   const tree = renderer.create(<Main />);
-  expect(tree.toJSON()).toMatchSnapshot();
-}); /*
+  const tree2 = renderer.create(<Main />).getInstance();
 
-  describe("Checking initial State", () => {
-    test("add new Note is closed", () => {
-      expect(tree.toTree().rendered.state.addNote).toBe(false);
-    });
+  test("renders correctly", () => {
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 
-    test("input Text is empty", () => {
-      expect(tree.toTree().rendered.instance.state.noteText).toBe("");
+  describe("Initial state", () => {
+    test("noteArray should be empty", () => {
+      expect(tree2.state.noteArray).toHaveLength(0);
     });
   });
 
   describe("Note functions", () => {
+    test("prevent empty notes from being added", () => {
+      tree2.addNote();
+      expect(tree2.state.noteArray).toEqual([]);
+    });
+
     test("add new note successfully", () => {
-      tree.toTree().rendered.instance.addNote();
-      expect(tree.toTree().rendered.instance.state.todoList).toHaveLength(1);
+      tree2.setState({ noteText: "do stuff" });
+      tree2.addNote();
+      expect(tree2.state.noteArray).toHaveLength(1);
     });
 
     test("delete note successfully", () => {
-      tree.toTree().rendered.instance.deleteNote(0);
-      expect(tree.toTree().rendered.instance.state.todoList).toHaveLength(0);
+      tree2.deleteNote(0);
+      expect(tree2.state.noteArray).toHaveLength(0);
+      expect(tree2.state.noteArray).toEqual([]);
     });
   });
-});*/
+});
--- components/__tests__/Todo-test.js
+++ components/__tests__/Todo-test.js
@@ -3,6 +3,6 @@ import Todo from "../Todo";
 import renderer from "react-test-renderer";
 
 test("Todo renders correctly", () => {
-  const tree = renderer.create(<Todo val="" />);
+  const tree = renderer.create(<Todo val={""} />);
   expect(tree.toJSON()).toMatchSnapshot();
 });
